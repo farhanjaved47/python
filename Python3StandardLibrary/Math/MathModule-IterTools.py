3f3192dcdebc3d696dca6dd8a2db6d8b61eaa48c
farhanjaved47 <farhan.javed47@gmail.com>
2019-12-30 17:14:26 +0500

    Python Maths - Itertools

--- Python3StandardLibrary/Math/MathModule-IterTools.py
+++ Python3StandardLibrary/Math/MathModule-IterTools.py
@@ -4,8 +4,13 @@ farhan.javed47@gmail.com
 12/15/2019
 Basic Math functionality in python 3 - IterTools module
 https://docs.python.org/3/library/math.html
+https://docs.python.org/3/library/itertools.html
 """
 import itertools
+
+
+def testFunction(x):
+    return x < 40
 
 
 def main():
@@ -39,6 +44,24 @@ def main():
     colors_for_painting = ['Red', 'Blue', 'Orange', 'Purple', 'Pink']
     for c in itertools.combinations(colors_for_painting, 2):
         print(c)
+
+    # Accumulate function accumulates values by default
+    vals = [10, 20, 30, 40, 50, 60, 40, 20, 10]
+    acc = itertools.accumulate(vals)
+    print(list(acc))
+    # Can find the max in the list
+    acc_max = itertools.accumulate(vals, max)
+    print(list(acc_max))
+    chained = itertools.chain("ABCD", "1234", "EFGH")
+    str1 = "This is a string"
+    str1 = str1.replace(" ", "")
+    convertToCharArray = itertools.chain(str1)
+    print(list(chained))
+    print(list(convertToCharArray))  # Can use this to convert a simple string to list of arrays too
+
+    # Dropwhile and takewhile will return values until a certain condition is met that stops them
+    print(list(itertools.dropwhile(testFunction, vals)))
+    print(list(itertools.takewhile(testFunction, vals)))
 
 
 if __name__ == '__main__': main()
